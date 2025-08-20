fc33453d848d530de3b5e0d466501d9fbb8feb42
Ahmadreza Shamimi <ahmadreza.shamimi@gmail.com>
2025-08-20 09:12:56 -0400

    Initial Commit

---
+++ crown loop Watch App/crown_loopApp.swift
@@ -0,0 +1,17 @@
+//
+//  crown_loopApp.swift
+//  crown loop Watch App
+//
+//  Created by Ahmadreza Shamimi on 2025-08-20.
+//
+
+import SwiftUI
+
+@main
+struct crown_loop_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
